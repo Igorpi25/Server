f331e9ffd343ed5eeb565fb742a6fcaa6037580f
uyscutiserver <uyscutiserver@gmail.com>
2016-05-25 21:35:13 +0900

    Setup URL igorpi25.ru/v1. Clean configs

--- Server/include/Config.php
+++ Server/include/Config.php
@@ -5,10 +5,10 @@
 
 //Файл публикуется с ошибками. Следует сразу добавить нужные строки 
 
-define('DB_USERNAME', 'igor');//MySql user login
-define('DB_PASSWORD', 'eY2vHmju');//Set password for mysql user
+define('DB_USERNAME', '***');//MySql user login
+define('DB_PASSWORD', '***');//Set password for mysql user
 define('DB_HOST', 'localhost');//You should set here your  own host name. Ususally used 'localhost'
-define('DB_NAME', 'igorserver');//date-base name used in mysql
+define('DB_NAME', '***');//date-base name used in mysql
  
 define('USER_CREATED_SUCCESSFULLY', 0);
 define('USER_CREATE_FAILED', 1);
@@ -20,11 +20,11 @@ define("size_full",600);
 define("size_avatar",300);
 define("size_icon",120);
 	
-define("path_images",'/igorserver/images');
+define("path_images",'/images');
 	
 define("path_fulls", path_images.'/fulls/');
 define("path_avatars", path_images.'/avatars/');
 define("path_icons", path_images.'/icons/');
 
-define("URL_HOME","http://94.245.159.1");//set here your domen home url
+define("URL_HOME","http://igorpi25.ru/v1");//set here your domen home url
 ?>
